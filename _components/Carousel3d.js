e4761556e22e478a089a87a79c9d571a55ab6082
Sebastian Silva <sebastian@fuentelibre.org>
2025-05-30 19:25:16 -0500

    Re-enable autoplay

--- _components/Carousel3d.js
+++ _components/Carousel3d.js
@@ -1,6 +1,6 @@
 import { Swiper } from 'https://cdn.jsdelivr.net/npm/swiper@10/+esm';
 import EffectCoverflow from 'https://cdn.jsdelivr.net/npm/swiper@10/modules/effect-coverflow.min.mjs';
-// import Autoplay from 'https://cdn.jsdelivr.net/npm/swiper@10/modules/autoplay.min.mjs';
+import Autoplay from 'https://cdn.jsdelivr.net/npm/swiper@10/modules/autoplay.min.mjs';
 import 'Carousel3d.css';
 
 function Carousel3({ items }) {
@@ -12,11 +12,11 @@ function Carousel3({ items }) {
       if (swiperRef.current) {
         const swiper = new Swiper(swiperRef.current, {
           effect: 'coverflow',
-          modules: [EffectCoverflow],
+          modules: [EffectCoverflow, Autoplay],
           centeredSlides: true,
           slidesPerView: 'auto',
           initialSlide: 1,
-          speed: 1800,
+          speed: 800,
           coverflowEffect: {
             rotate: 30,
             scale: 0.8,
@@ -28,11 +28,11 @@ function Carousel3({ items }) {
             el: '.swiper-pagination',
             clickable: true,
           },
-          /* autoplay: {
+          autoplay: {
             delay: 4000,
             disableOnInteraction: true,
             pauseOnMouseEnter: true
-          },*/
+          },
         });
         swiper.on('slideChange', () => {
           console.log('Slide changed to index:', items[swiper.activeIndex].title);
